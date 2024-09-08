0e1128e80b4ca84422a4309583e3d55edcc77223
02shreyansh <shreyansh2102004@gmail.com>
2024-09-08 23:21:51 +0530

    added the music source

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Cards = ({title,images,name="Shreyansh"}) => {
-    
+const Cards = ({title,images,sounds,name="Shreyansh"}) => {
+    const [isPlaying,setIsPlaying]=useState(false);
+    const playSound=()=>{
+        const audio=new Audio(sounds);
+        setIsPlaying(true);
+        audio.play();
+        audio.onended=()=>{
+            setIsPlaying(false);
+        }
+    }
   return (
     <div className="my-2 px-1 mr-2 w-full md:w-60  lg:px-4 lg:w-72 border-2 border-white bg-gray-800 rounded-3xl ">
         <article className="overflow-hidden rounded-lg shadow-lg">
-            <button className='bg-white h-48 w-48 lg:ms-9 ms-16 mt-1 md:ms-10'>
-                <img alt="Placeholder" className=" h-full w-full " src={images}/>
+            <button className='bg-white h-48 w-48 lg:ms-9 ms-16 mt-1 md:ms-10'
+            onClick={playSound}>
+                <img alt="Placeholder" className=" h-full w-full " src={images} draggable={false}/>
             </button>
             <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                 <h1 className="text-lg">
@@ -15,7 +24,7 @@ const Cards = ({title,images,name="Shreyansh"}) => {
                     </h4>
                 </h1>
                 <p className="text-white text-sm">
-                    Drum-Set
+                    {isPlaying ? "Playing..." : "Drum-Set"}
                 </p>
             </header>
             <footer className="flex items-center justify-between leading-none p-2 md:p-4">
--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -3,25 +3,34 @@ import Cards from './Cards'
 import '../Css/Player.css'
 const Player = () => {
     const images={
-        crash:"src/assets/images/crash.png",
-        kick:"src/assets/images/kick.png",
-        snare:"src/assets/images/snare.png",
-        tom_1:"src/assets/images/tom1.png",
-        tom_2:"src/assets/images/tom2.png",
-        tom_3:"src/assets/images/tom3.png",
-        tom_4:"src/assets/images/tom4.png",
+        crash:"public/assets/images/crash.png",
+        kick:"public/assets/images/kick.png",
+        snare:"public/assets/images/snare.png",
+        tom_1:"public/assets/images/tom1.png",
+        tom_2:"public/assets/images/tom2.png",
+        tom_3:"public/assets/images/tom3.png",
+        tom_4:"public/assets/images/tom4.png",
     };
+    const Sounds={
+        crash:"public/assets/Sounds/crash.mp3",
+        kick:"public/assets/Sounds/kick-bass.mp3",
+        snare:"public/assets/Sounds/snare.mp3",
+        tom_1:"public/assets/Sounds/tom-1.mp3",
+        tom_2:"public/assets/Sounds/tom-2.mp3",
+        tom_3:"public/assets/Sounds/tom-3.mp3",
+        tom_4:"public/assets/Sounds/tom-4.mp3",
+    }
   return (
     <div className='w-full h-full duration-200  bg-cover bg-center first'>
         <div className="container  mx-auto px-4 md:px-12">
             <div className="flex flex-wrap -mx-1 lg:-mx-4">
-                <Cards title="Crash" images={images.crash}/>
-                <Cards title="Kick" images={images.kick}/>
-                <Cards title="Snare" images={images.snare}/>
-                <Cards title="Tom-1" images={images.tom_1}/>
-                <Cards title="Tom-2" images={images.tom_2}/>
-                <Cards title="Tom-3" images={images.tom_3}/>
-                <Cards title="Tom-4" images={images.tom_4}/>
+                <Cards title="Crash" images={images.crash} sounds={Sounds.crash}/>
+                <Cards title="Kick" images={images.kick} sounds={Sounds.kick}/>
+                <Cards title="Snare" images={images.snare} sounds={Sounds.snare}/>
+                <Cards title="Tom-1" images={images.tom_1} sounds={Sounds.tom_1}/>
+                <Cards title="Tom-2" images={images.tom_2} sounds={Sounds.tom_2}/>
+                <Cards title="Tom-3" images={images.tom_3} sounds={Sounds.tom_3}/>
+                <Cards title="Tom-4" images={images.tom_4} sounds={Sounds.tom_4}/>
             </div>
         </div>
     </div>
